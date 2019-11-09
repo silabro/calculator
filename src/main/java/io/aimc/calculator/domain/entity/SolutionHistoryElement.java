dbca20c81666048dac8273e59adb1fdb38921967
silabro <pashkamen4ik@gmail.com>
2019-11-09 19:33:11 +0600

    Added builder in SolutionHistoryElement

--- src/main/java/io/aimc/calculator/domain/entity/SolutionHistoryElement.java
+++ src/main/java/io/aimc/calculator/domain/entity/SolutionHistoryElement.java
@@ -1,5 +1,6 @@
 package io.aimc.calculator.domain.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 @Table(name = "solution_history")
 @Getter
 @Setter
+@Builder
 public class SolutionHistoryElement {
 
     @Id
